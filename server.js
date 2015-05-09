f2a677ecb8aec884f75a4e84759319feaa1e2a92
gcoutable <guillaume.coutable@gmail.com>
2015-05-09 18:58:59 +0200

    Add markers and infowindow on markers

---
+++ server.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+var staticPath = path.resolve(__dirname, '/app');
+app.use(express.static(staticPath));
+
+app.listen(8080, function() {
+  console.log(app);
+  console.log('listening');
+});
